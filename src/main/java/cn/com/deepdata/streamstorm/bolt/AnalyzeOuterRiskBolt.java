8e540965ccb837897bc6d518440326a4558fafae
ykh <ykh@deepdata.com.cn>
2016-11-15 16:23:50 +0800

    CRLF ---> LF

---
+++ src/main/java/cn/com/deepdata/streamstorm/bolt/AnalyzeOuterRiskBolt.java
@@ -0,0 +1,7 @@
+package cn.com.deepdata.streamstorm.bolt;
+
+/**
+ * Created by yukh on 2016/11/15
+ */
+public class AnalyzeOuterRiskBolt {
+}
--- src/main/java/cn/com/deepdata/streamstorm/entity/InnerRiskValue.java
+++ src/main/java/cn/com/deepdata/streamstorm/entity/InnerRiskValue.java
@@ -8,7 +8,7 @@ import java.util.List;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 
-public class InnerRiskValue {
+public class InnerRiskValue implements Entity {
 	public List<ClientScore> clientScore;
 	public List<DescRiskScore> riskScore;
 	public List<Tag> tag;
